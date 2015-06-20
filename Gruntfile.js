e37407fd41a33c6032eb29547714fdf9fef07b54
Jakub Korupczynski <jakub.korupczynski@gmail.com>
2015-06-20 14:53:13 +0100

    Cleaning Gruntfile.js and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,9 +9,9 @@ module.exports = function(grunt) {
       test: {
         options: {
           reporter: 'spec',
-          captureFile: 'results.txt', // Optionally capture the reporter output to a file
-          quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+          captureFile: 'results.txt',
+          quiet: false,
+          clearRequireCache: false
         },
         src: ['test/**/*.js']
       }
